b3b0609eb5748bcf44043dede70d3b8d4278aca3
Chris Rockwell <74079689+Cypher1520@users.noreply.github.com>
2024-07-18 09:26:44 -0600

    Cleanup

--- Defender/Celero/Get-ADServers.ps1
+++
@@ -1,3 +0,0 @@
-Get-ADComputer -Filter {OperatingSystem -like "*windows*server*"} -Properties DNSHostName, OperatingSystem,IPv4Address | sort DNSHostname
-
-Get-ADComputer -Filter { OperatingSystem -like "*server*" } -Properties * | select Name,OperatingSystem,OperatingSystemVersion,CanonicalName | Export-Csv C:\Temp\MDE\ADServers.csv
--- Defender/Celero/PolicyCLI.ps1
+++
@@ -1,3 +0,0 @@
-az account show
-az account set --subscription "SUB-Caisse-ManagedApplications-PRD"
-az policy state trigger-scan
